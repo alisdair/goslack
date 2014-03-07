fad4accca947b9b6ddf229fbd2426ee94abb3458
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2014-03-07 19:41:10 +0000

    Display the pipeline stage in Slack messages.

--- slackMessage.js
+++ slackMessage.js
@@ -1,12 +1,23 @@
 var strftime = require('strftime');
 
+var findStatus = function(categories) {
+  return categories.filter(function(c) {
+    return c == 'passed' || c == 'failed'
+  })[0];
+}
+
+var findStage = function(title) {
+  var matches = title.match(/stage (\w+)/);
+
+  if (matches) {
+    return matches[1] + ' ';
+  } else {
+    return '';
+  }
+};
+
 // Convert a feed item into a slack message
 var message = function(item) {
-  var findStatus = function(categories) {
-    return categories.filter(function(c) {
-      return c == 'passed' || c == 'failed'
-    })[0];
-  }
 
   var decorator = {
     passed: ':thumbsup:',
@@ -14,16 +25,17 @@ var message = function(item) {
     unknown: ':horse:'
   }
 
-  var title, status, link, date, author;
+  var title, stage, status, link, date, author;
 
   title = item.meta.title || item.title;
+  stage = findStage(item.title);
   status = findStatus(item.categories) || "unknown";
   link = item.link;
   date = strftime("%H:%M %b %d", item.date);
   author = item.author;
 
-  return '<' + link + '|Build for ' + title + '>: ' +
+  return '<' + link + '|Build for ' + title + '>: ' + stage +
     decorator[status] + ' ' + status + ' (' + date + ")\n" + author;
-}
+};
 
 module.exports = message;
